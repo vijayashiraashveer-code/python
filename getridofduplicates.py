bb408eb405f1d3648690a07c299bdd0418a54f79
vijayashiraashveer-code <vijayashiraashveer@gmail.com>
2026-07-22 09:51:15 +0530

    Add files via upload

---
+++ getridofduplicates.py
@@ -0,0 +1,10 @@
+student_data={
+    "id1":{"name": "Sara", "class":"5th", "subject":English"},
+     "id2":{"name": "David", "class":"5th", "subject":Math"},
+     "id3":{"name": "Sara", "class":"5th", "subject":English"},
+     "id4":{"name": "Shourya", "class":"5th", "subject":science"},
+}
+
+seen=set()
+ for k,v in student_data.items():
+    
