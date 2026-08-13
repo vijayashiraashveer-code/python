bb41d46a64f01284c61822c1846092b9c6f01f0d
vijayashiraashveer-code <vijayashiraashveer@gmail.com>
2026-08-13 16:57:42 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,11 @@
+class element:
+    def  twosun(self,nums,target):
+        lookup={}
+        for i,nums in enumerate(nums):
+            return(lookup[target-nums],i)
+        lookup[nums]=i
+value=int(input())
+obj=element()
+print(obj.twosun)
+
+        
