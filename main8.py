a052b631f5cd1d7d67fd5ceb06a7fe8e0e11cbef
vijayashiraashveer-code <vijayashiraashveer@gmail.com>
2026-09-03 16:40:52 +0530

    Add files via upload

---
+++ main8.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+class animal(ABC):
+    def move(self):
+        pass
+class human(Animal):
+
+    def move(self):
+        print("I can walk and run")
+
+class snake(animal):
+    def move(self):
+        print("i can crawl")
+
+class dog(animal):
+
+    def move("self")
+        print("i can bark")
+class lion(animal):
+
+    def move("self")
+        print("i can roar")
+R = human()
+R. move()    
+K = snake()
+K. Move()
+R = dog()
+R.move()
+K =Lion()
+k.move()
+   
