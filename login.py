e8ea2acbac1d7dbb413469b6f002940590149bf0
vijayashiraashveer-code <vijayashiraashveer@gmail.com>
2026-09-22 16:45:10 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -4,11 +4,13 @@ pygame.init()
 # setup window geometry
 screen = pygame.display.set_mode((400,500))
 # create a loop to run til the game is quit by the user
+pygame.display.set_caption('ading image and baground image')
+baground image = pygame.transform.scale pygame.image.load
 done = False 
 while not done :
     for event in pygame.event.get():
         if event.i==event.quit:
             pygame.quit()
     pygame.display.flip()
-    
+
 
