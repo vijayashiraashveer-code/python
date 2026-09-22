c3118c601d1e507936eaf9c94e571a69b85ffe2a
vijayashiraashveer-code <vijayashiraashveer@gmail.com>
2026-09-22 16:36:46 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,14 @@
+import pygame #import neccesary libraries
+# initalize required mo
+pygame.init()
+# setup window geometry
+screen = pygame.display.set_mode((400,500))
+# create a loop to run til the game is quit by the user
+done = False 
+while not done :
+    for event in pygame.event.get():
+        if event.i==event.quit:
+            pygame.quit()
+    pygame.display.flip()
+    
+
